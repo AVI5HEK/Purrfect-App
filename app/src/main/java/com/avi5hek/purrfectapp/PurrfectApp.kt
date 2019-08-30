d27057fb2aecf9a4de5d4cf89fb21ebecd1ed36d
Avishek Khan <avishek.khan@sekai-lab-bd.com>
2019-08-30 20:20:39 +0600

    Creating a custom Application class

--- app/src/main/java/com/avi5hek/purrfectapp/PurrfectApp.kt
+++ app/src/main/java/com/avi5hek/purrfectapp/PurrfectApp.kt
@@ -1,11 +1,22 @@
 package com.avi5hek.purrfectapp
 
 import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
 /**
  * Created by "Avishek" on 8/19/2019.
  */
-class PurrfectApp : Application() {
+class PurrfectApp : Application(), HasAndroidInjector {
+
+  @Inject
+  lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+  override fun androidInjector(): AndroidInjector<Any> {
+    return dispatchingAndroidInjector
+  }
 
   override fun onCreate() {
     super.onCreate()
